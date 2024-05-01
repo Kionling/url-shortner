b11ad1f9d76487a611908ed1224d1d2dec2d7533
Daniel Jauregui <djvdjv209@gmail.com>
2024-04-30 22:40:43 -0700

    Defined function taht will retrieve random string

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ from flask import Flask, request, redirect, render_template, url_for
 import sqlite3
 import random
 import string
+
+app = Flask(__name__)
+
+
+def get_random_string(length=6):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for i in range(length))
