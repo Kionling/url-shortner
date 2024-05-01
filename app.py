3bbcb2e88c36eb80abea848086c72abb8d9aebe4
Daniel Jauregui <djvdjv209@gmail.com>
2024-04-30 22:41:36 -0700

    Initializing connection with DB

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ app = Flask(__name__)
 def get_random_string(length=6):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for i in range(length))
+
+
+def init_db():
+    conn = sqlite3.connect('urls.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS urls (short TEXT, original TEXT)')
+    conn.commit()
+    conn.close()
